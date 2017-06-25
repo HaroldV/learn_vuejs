1fe2d3441933242a001eac542148606cd7973906
haroldv22 <haroldestebanvillalobos@gmail.com>
2017-06-25 19:45:32 -0400

    Ejercicio con Modal

--- js/laracast-exercise-modal.js
+++ js/laracast-exercise-modal.js
@@ -1,15 +1,15 @@
 Vue.component('modal',{
 
-  template: '<div class="modal is-active">\
+  template: `<div class="modal is-active">\
                 <div class="modal-background"></div>\
                 <div class="modal-content">\
                   <div class="box">\
                     Any other Bulma elements you want\
                   </div>\
-                </div>\
-                <button class="modal-close" @click="$emit(close)">x</button>\
+                </div>
+                <button class="modal-close" @click="$emit('close')">x</button>\
               </div>\
-            </div>'
+            </div>`
 })
 
 new Vue({
